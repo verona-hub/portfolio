3ab956562d78bd92fb54ed17c6940f75981e99d3
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-10-31 20:26:19 +0100

    Changes: - fixing error on TypewritingFunc.js

--- src/components/Typewriting/TypewritingFunc.js
+++ src/components/Typewriting/TypewritingFunc.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 
 const TypewritingFunc = () => {
+    let header = document.querySelector('.typewriting');
 
-    const texts = ['Hello.', 'I am Zoran.', 'I am a junior web developer'];
+    const texts = ['I am Zoran.', 'I am a junior web developer'];
     let count = 0;
     let index = 0;
     let currentText = '';
     let letter = '';
 
-    function type () {
+    (function type () {
         // If count is equal the array length
         if(count === texts.length) {
             // Reset to zero
@@ -20,23 +21,24 @@ const TypewritingFunc = () => {
         // Increment one character at the time
         letter = currentText.slice(0, ++index);
 
-        document.querySelector('.typewriting').textContent = letter;
+        // document.querySelector('.typewriting').textContent = letter;
+        // header.textContent = letter;
 
         if(letter.length === currentText.length) {
             count++;
             index = 0;
         }
         setTimeout(type, 400);
-    }
+    }());
 
-    type();
+    // console.log(header.textContent)
 
     // window.onload = function(e){
     //     console.log(e)
     // }
 
     return (
-        <h1 className="typewriting"> </h1>
+        <h1 className="typewriting"> Hello </h1>
     );
 }
 
