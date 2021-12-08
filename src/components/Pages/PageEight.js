730acbbbd0bad97789101b82fddbdaa2a3f34e9c
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-12-08 04:25:47 +0100

    Changes: - updated the game

--- src/components/Pages/PageEight.js
+++ src/components/Pages/PageEight.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const PageEight = () => {
+    const cells = document.querySelectorAll('.cell');
+    const xTurn = 'x';
+    const oTurn = 'o';
+
+    // State
+    const [turn, setTurn] = useState('x');
+
+
+
+    function handleClick(e) {
+        // xTurn && setTurn('x');
+        // oTurn && setTurn('o');
+        cells.forEach(cell => {
+            cell.addEventListener('click', handleClick, { once: true });
+        });
+
+        if (turn === 'x') {
+            e.target.classList.add('x');
+            setTurn('o');
+        } else {
+            e.target.classList.add('circle');
+            setTurn('x');
+        }
+
+
+        console.log(turn)
+        console.log(e.target)
+    }
 
     return (
         <div id='page-eight-Game' className='Page page-eight'>
-            <div id='board' className='board x'>
-                <div className='cell x'> </div>
-                <div className='cell circle'> </div>
+            <h2> Turn: { turn } </h2>
+            <div id='board' className='board x' onClick={ handleClick }>
+                <div className='cell'> </div>
+                <div className='cell'> </div>
                 <div className='cell'> </div>
                 <div className='cell'> </div>
                 <div className='cell'> </div>
