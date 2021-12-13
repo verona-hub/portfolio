482402062d93bd39ce254c903ed1a9f4a1a0353a
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-12-13 20:31:44 +0100

    Changes: - updated the game: added hover classes to the board (bug to fix: first cell does not have the hover since it is inside the startGame function)

--- src/components/Pages/PageEight.js
+++ src/components/Pages/PageEight.js
@@ -5,38 +5,60 @@ const PageEight = () => {
 
     // State
     const [turn, setTurn] = useState('x');
+
     const cells = [1,2,3,4,5,6,7,8,9];
 
+
     const startGame = (e) => {
-        const cell = e.target;
+        const board = document.querySelector('.board');
+        const currentCell = e.target;
         const errorMessage = document.querySelector('.error-message');
         const errorButton = document.getElementById('error-button');
 
-        const player = {
+        const cell = {
             x: { turn: 'x', class: 'x' },
-            o: { turn: 'o', class: 'circle' },
+            circle: { turn: 'o', class: 'circle' },
             includesEitherClass(){
-                return cell.classList.contains(this.x.class) || cell.classList.contains(this.o.class);
+                return currentCell.classList.contains(this.x.class) || currentCell.classList.contains(this.circle.class);
             },
-            errorText: 'The cell is already occupied'
-        }
-
-        if (turn === player.x.turn || turn === player.o.turn) {
-            if(player.includesEitherClass()){
-                errorMessage.classList.toggle('show');
-
+            toggleErrorMessage(){
                 errorButton.onclick = () => {
                     errorMessage.classList.toggle('show');
-                }
-            }
-            else if (turn === player.x.turn) {
-                cell.classList.add(player.x.class);
-                setTurn(player.o.turn);
-            } else {
-                cell.classList.add(player.o.class);
-                setTurn(player.x.turn);
+                };
+                return errorMessage.classList.toggle('show')
             }
         }
+
+        if (turn === cell.x.turn || turn === cell.circle.turn) {
+            if(cell.includesEitherClass()){
+                cell.toggleErrorMessage();
+            }
+            else if (turn === cell.x.turn) {
+                currentCell.classList.add(cell.x.class);
+                setTurn(cell.circle.turn);
+            } else {
+                currentCell.classList.add(cell.circle.class);
+                setTurn(cell.x.turn);
+            }
+        }
+
+        const hoverClass = () => {
+            board.classList.remove(cell.x.class);
+            board.classList.remove(cell.circle.class);
+
+            if(turn === cell.x.turn){
+                board.classList.add(cell.circle.class);
+            } else {
+                board.classList.add(cell.x.class);
+            }
+        }
+        hoverClass();
+
+        // const resetButton = document.getElementById('reset-button');
+        // resetButton.addEventListener('click', event => {
+        //     startGame();
+        // });
+
     }
 
     return (
@@ -53,17 +75,18 @@ const PageEight = () => {
             </div>
 
             <div className='restart'>
-                <button id='restart'> Restart </button>
+                <button id='reset-button'> Restart </button>
             </div>
 
             <div className='win-message'>
                 <div><h1> You won!!! </h1></div>
-                <button id='win-message'> Reset </button>
+                <button id='reset-button2'> Reset </button>
             </div>
 
             <div className='error-message'>
-                <h2> This cell is occupied. Try a different one. </h2>
-                <button id='error-button'> Click here to continue </button>
+                <h2> This cell is occupied. </h2>
+                <h2> Try a different one. </h2>
+                <button id='error-button'> Continue </button>
             </div>
         </div>
     );
