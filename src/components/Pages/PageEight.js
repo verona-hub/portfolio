de6735641fa917820ca0a88e52a8169ef3703d84
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-12-15 01:12:09 +0100

    Changes: - updated the game: refactored code, separated the hover function

--- src/components/Pages/PageEight.js
+++ src/components/Pages/PageEight.js
@@ -8,87 +8,72 @@ const PageEight = () => {
 
     const cellElements = [1,2,3,4,5,6,7,8,9];
 
-    // const cells = document.querySelectorAll('.cell');
-    // cells.forEach( cell => {
-    //     cell.addEventListener('click', cellClick );
-    // });
+    const errorButton = document.getElementById('error-button');
+    const restartButton = document.getElementById('restart-button');
+    const errorMessage = document.querySelector('.error-message');
 
     // TODO
     // Do you want to play a game ? component appears : null
     // Move everything inside this function
     const startGame = () => {
-        console.log('The game started')
-    }
+        // console.log('The game started');
+    };
+
+    const cell = {
+        x: { turn: 'x', class: 'x' },
+        circle: { turn: 'o', class: 'circle' }
+    };
 
     const cellClick = (e) => {
-        console.log('cell is active')
-        const errorMessage = document.querySelector('.error-message');
-        const errorButton = document.getElementById('error-button');
+        const currentCell = e.target;
+        // Place the mark
+        placeMark(currentCell);
+        // Start the hover effect
+        hoverShadow();
+    };
 
-        const cell = {
-            x: { turn: 'x', class: 'x' },
-            circle: { turn: 'o', class: 'circle' },
-            current: e.target,
-            includesEitherClass(){
-                return this.current.classList.contains(this.x.class) || this.current.classList.contains(this.circle.class);
-            },
-            toggleErrorMessage(){
-                errorButton.onclick = () => {
-                    errorMessage.classList.toggle('show');
-                };
-                return errorMessage.classList.toggle('show')
-            }
-        }
+    const placeMark = (currentCell) => {
+        const includesEitherClass = currentCell.classList.contains(cell.x.class) || currentCell.classList.contains(cell.circle.class);
 
-        const restartButton = document.getElementById('restart-button');
-        restartButton.addEventListener('click', () => {
-            console.log('restart game')
-        });
-
-        placeMark(cell);
-        hoverShadow(cell);
-    }
-
-    function placeMark(cell){
-        console.log(cell.current)
-        if (turn === cell.x.turn || turn === cell.circle.turn) {
-            if(cell.includesEitherClass()){
-                cell.toggleErrorMessage();
-            } else {
-                if (turn === cell.x.turn) {
-                    cell.current.classList.add(cell.x.class);
-                    switchTurn();
-                } else {
-                    cell.current.classList.add(cell.circle.class);
-                    switchTurn();
-                }
-            }
-        }
-    }
-
-    function switchTurn() {
-        if(turn === 'x'){
-            setTurn('o');
+        if (includesEitherClass){
+            showErrorMessage();
         } else {
-            setTurn('x')
+            turn === cell.x.turn
+                ? currentCell.classList.add(cell.x.class)
+                : currentCell.classList.add(cell.circle.class);
+            switchTurn();
         }
-    }
+    };
 
-    function hoverShadow(cell){
+    const switchTurn = () => {
+        turn === cell.x.turn
+            ? setTurn(cell.circle.turn)
+            : setTurn(cell.x.turn);
+    };
+
+    const hoverShadow = () => {
         const board = document.querySelector('.board');
         board.classList.remove(cell.x.class);
         board.classList.remove(cell.circle.class);
 
-        if(turn === cell.x.turn){
-            board.classList.add(cell.circle.class);
-        } else {
-            board.classList.add(cell.x.class);
-        }
-    }
+        turn === cell.x.turn
+            ? board.classList.add(cell.circle.class)
+            : board.classList.add(cell.x.class);
+    };
+
+    const showErrorMessage = () => {
+        errorButton.onclick = () => {
+            restartButton.addEventListener('click', () => {
+                console.log('restart game');
+            });
+            errorMessage.classList.toggle('show');
+        };
+        return errorMessage.classList.toggle('show');
+    };
 
     return (
         <div id='page-eight-Game' className='Page Game page-eight'>
-            <button onClick={ () => startGame }> Start the game </button>
+            <button onClick={ () => startGame() }> Start the game </button>
             <h2> Turn: { turn } </h2>
             <div id='board' className='board'>
                 {
